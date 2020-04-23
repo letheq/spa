f417f1f50ffcbbdd5cfdca9a47230e6a60a24671
Bob Bobicek <bob.bobicek@centrum.cz>
2020-04-23 13:29:24 +0200

    oprava č.3

--- api-chat.js
+++ api-chat.js
@@ -1,15 +1,20 @@
 const dateFormat = require('dateformat');
 
 let msgs = new Array();
+let nicks = new Array();
 
 exports.apiChat = function (req, res, obj) {
     if (req.pathname.endsWith("/list")) {
         obj.messages = msgs;
+        obj.nicknames = nicks;
     } else if (req.pathname.endsWith("/add")) {
         msgs.push();
+        nicks.push();
         let obj = {};
         obj.message = req.parameters.msg;
+        obj.nickname = req.parameters.nick;
         obj.time = dateFormat(new Date(), "dd.mm.yyyy HH:MM:ss");
         msgs.push(obj);
+        nicks.push(obj);
     }
 }
