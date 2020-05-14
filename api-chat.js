3f92fe9a9eaaaa50afa6b3406197e39bdd28cc46
Bob Bobicek <bob.bobicek@centrum.cz>
2020-05-14 13:39:02 +0200

    oprava č.č

--- api-chat.js
+++ api-chat.js
@@ -1,20 +1,23 @@
 const dateFormat = require('dateformat');
+const fs = require('fs');
+let nicks = [];
+let msgs = [];
+const MSGS_FILE = "chat.json"
 
-let msgs = new Array();
-let nicks = new Array();
+if (fs.existsSync(MSGS_FILE)){
+    msgs = JSON.parse(fs.readFileSync(MSGS_FILE));
+    }
 
 exports.apiChat = function (req, res, obj) {
     if (req.pathname.endsWith("/list")) {
         obj.messages = msgs;
         obj.nicknames = nicks;
     } else if (req.pathname.endsWith("/add")) {
-        msgs.push();
-        nicks.push();
         let obj = {};
         obj.message = req.parameters.msg;
         obj.nickname = req.parameters.nick;
         obj.time = dateFormat(new Date(), "dd.mm.yyyy HH:MM:ss");
         msgs.push(obj);
         nicks.push(obj);
-    }
+        fs.writeFileSync(MSGS_FILE, JSON.stringify(msgs,null,2));
 }
